a7f8d5f8aca3f7d2ffba429574b83352b3282bea
Akshay Prakash Kolhe <129017430+UserAK04@users.noreply.github.com>
2025-01-06 11:31:03 +0530

    Array Problem

---
+++ Arrays/ClosestToZero.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/find-closest-number-to-zero/description/?envType=problem-list-v2&envId=array&difficulty=EASY&status=TO_DO
+
+# Approach:
+# Traverse accross the array
+# Take absolute value of each element & store the smallest & it's signed value as well
+# that is, store absolute smallest element & it's signed value in 2 variables
+
+# If same val is present in -ve & +ve form in the array then return +ve one
+
+
+def solve(nums):
+    closest = 1e9
+    val = 0
+
+    for ele in nums:
+        if abs(ele) < closest:
+            closest = abs(ele)
+            val = ele
+        elif abs(ele) == closest:
+            val = max(ele, val)
+
+    return val
+
+
+# Edge Cases
+# nums = [0]
+# nums = [-100000, -100000]
+nums = [-1, 1]
+
+print(solve(nums))
