0edd8547662c674e46428a1d6c10bb5ba2732722
Akshay Prakash Kolhe <129017430+UserAK04@users.noreply.github.com>
2025-01-11 11:13:57 +0530

    Update MissingInArray.py

--- Arrays/MissingInArray.py
+++ Arrays/MissingInArray.py
@@ -1,9 +1,14 @@
+# https://leetcode.com/problems/missing-number/submissions/1504732493/
+
+# Approach:
+# Take Sum of first n natural no.s
+# Traverse & Take the sum of missingArray
+# Difference btw the first sum and second sum is the missing Element
+
+# Revision - |
+
 def missingNumber(arr):
-
-    # Take Sum of first n natural no.s
-    # Traverse & Take the sum of missingArray
-    # Difference btw the first sum and second sum is the missing Element
-
+    
     n = len(arr) + 1
     sum = n * (n + 1) // 2
     minSum = 0
@@ -12,6 +17,5 @@ def missingNumber(arr):
         minSum += ele
 
     return sum - minSum
-
 
 print(missingNumber([]))
