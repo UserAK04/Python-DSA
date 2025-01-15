fa0974905f50eb72c4df3fbb41ec5159bfd5a095
Akshay Prakash Kolhe <129017430+UserAK04@users.noreply.github.com>
2025-01-15 10:41:22 +0530

    String Problem

---
+++ Strings/CountSeniors.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/number-of-senior-citizens/description/?envType=problem-list-v2&envId=array&difficulty=EASY&status=TO_DO
+
+# Approach:
+# Convert the value at index 11 and 12 to a numerical value.
+
+
+# Input:
+details = ["7868190130M7522", "5303914400F9211", "9273338290F4010"]
+# Output: 2
+
+# Input:
+# details = ["1313579440F2036", "2921522980M5644"]
+# Output: 0
+
+
+def countSeniors(details):
+
+    seniorsCnt = 0
+    for detail in details:
+        if int(detail[11:13]) > 60:
+            cntSeniors += 1
+
+    return cntSeniors
+
+
+print(countSeniors(details))
