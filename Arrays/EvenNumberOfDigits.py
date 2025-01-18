931d532bc2a79c7d79d9e4a3a6b44fb14ff44828
Akshay Prakash Kolhe <129017430+UserAK04@users.noreply.github.com>
2025-01-18 23:46:35 +0530

    Update EvenNumberOfDigits.py

--- Arrays/EvenNumberOfDigits.py
+++ Arrays/EvenNumberOfDigits.py
@@ -1,4 +1,4 @@
-# https://leetcode.com/problems/find-numbers-with-even-number-of-digits/description/?envType=problem-list-v2&envId=array&difficulty=EASY&status=TO_DO
+# https://leetcode.com/problems/find-numbers-with-even-number-of-digits/description/
 
 # Approach:
 # Check constraints like size maximum size of digits given
