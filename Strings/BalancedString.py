62d6b06a6ca6ea47c8aac25d55b3db149caf7b96
Akshay Prakash Kolhe <129017430+UserAK04@users.noreply.github.com>
2025-01-25 10:35:53 +0530

    String Problem

---
+++ Strings/BalancedString.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/check-balanced-string/?envType=problem-list-v2&envId=string
+
+# Input:
+num = "1234"
+# Output: false
+
+# Input:
+# num = "24123"
+# Output: true
+
+
+def solve(num):
+
+    evenSum = 0
+    oddSum = 0
+
+    for i in range(len(num)):
+        if i % 2 == 0:
+            evenSum += int(num[i])
+        else:
+            oddSum += int(num[i])
+
+    return evenSum == oddSum
+
+
+print(solve(num))
