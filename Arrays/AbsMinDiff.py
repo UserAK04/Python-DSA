a80e39532b55d98bb2ef815955990cc004d43e7f
Akshay Prakash Kolhe <129017430+UserAK04@users.noreply.github.com>
2025-01-30 19:35:01 +0530

    Update AbsMinDiff.py

--- Arrays/AbsMinDiff.py
+++ Arrays/AbsMinDiff.py
@@ -1,4 +1,4 @@
-# https://leetcode.com/problems/minimum-absolute-difference/description/?envType=problem-list-v2&envId=array&difficulty=EASY&status=TO_DO
+# https://leetcode.com/problems/minimum-absolute-difference/description/
 
 # Revision - |
 
