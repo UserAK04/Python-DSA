3cfb5275f27f221568819a84b7a6def32297198d
Akshay Prakash Kolhe <129017430+UserAK04@users.noreply.github.com>
2025-01-31 14:26:06 +0530

    Update FindSubstring.py

--- Strings/FindSubstring.py
+++ Strings/FindSubstring.py
@@ -1,4 +1,4 @@
-# https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/?envType=problem-list-v2&envId=string
+# https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
 
 # Approach:
 # Using pointer, traverse the haystack string till n - length of needle
