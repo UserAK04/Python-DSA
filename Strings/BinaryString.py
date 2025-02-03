51ecd89d5e486429ac2916ada02dca1699482c57
Akshay Prakash Kolhe <129017430+UserAK04@users.noreply.github.com>
2025-02-03 14:16:30 +0530

    String Problem

---
+++ Strings/BinaryString.py
@@ -0,0 +1,26 @@
+# https://www.geeksforgeeks.org/problems/binary-string-1587115620/1?page=2&category=Strings&difficulty=Basic,Easy&status=unsolved&sortBy=submissions
+
+# Approach:
+# [ n * (n-1) / 2 ]
+# Is the formula of Combinatrics
+# Which is used to calculate
+# Number of ways in which unique pairs can be formed
+
+# Input:
+# N = 4
+# S = 1111
+# Output: 6
+
+# Input:
+n = 5
+s = "01101"
+# Output: 3
+
+
+def binarySubstring(n, s):
+
+    c = s.count("1")
+    return int(c * (c - 1) // 2)
+
+
+print(binarySubstring(n, s))
