0740294d41c669b36a7f13aa5dcdd33a84157236
Akshay Prakash Kolhe <129017430+UserAK04@users.noreply.github.com>
2025-02-03 13:49:50 +0530

    String Problem

---
+++ Strings/FizzBuzz.py
@@ -0,0 +1,33 @@
+# https://leetcode.com/problems/fizz-buzz/description/?envType=problem-list-v2&envId=string
+
+# Input:
+# n = 3
+# Output: ["1","2","Fizz"]
+
+# Input:
+# n = 5
+# Output: ["1","2","Fizz","4","Buzz"]
+
+# Input:
+n = 15
+# Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+
+
+def solve(n):
+
+    answer = []
+    for i in range(1, n + 1):
+
+        if i % 3 == 0 and i % 5 == 0:
+            answer.append("FizzBuzz")
+        elif i % 3 == 0:
+            answer.append("Fizz")
+        elif i % 5 == 0:
+            answer.append("Buzz")
+        else:
+            answer.append(str(i))
+
+    return answer
+
+
+print(solve(n))
